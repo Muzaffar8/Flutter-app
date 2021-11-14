8e7a35f512121ac5750955275d291ab47fac919a
Muzaffar8 <muzaffar.tursunbaev@iaau.edu.kg>
2021-11-14 23:11:56 +0600

    first commit

---
+++ lib/card5.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+
+import 'fooderlich_theme.dart';
+
+class Card5 extends StatelessWidget {
+  const Card5({Key? key}) : super(key: key);
+  // 1
+  final String category = 'Editor\'s Choice';
+  final String title = 'The Art making Pizza';
+  final String description = 'Learn to make the perfect pizza everytime.';
+  final String chef = 'Muza';
+
+  // 2
+  @override
+  Widget build(BuildContext context) {
+    // 3
+    return Center(
+      child: Container(
+        child: Stack(
+          children: [
+            // 8
+            Text(
+              category,
+              style: FooderlichTheme.darkTextTheme.bodyText1,
+            ),
+            // 9
+            Positioned(
+              child: Text(
+                title,
+                style: FooderlichTheme.darkTextTheme.headline2,
+              ),
+              top: 20,
+            ),
+            // 10
+            Positioned(
+              child: Text(
+                description,
+                style: FooderlichTheme.darkTextTheme.bodyText1,
+              ),
+              bottom: 30,
+              right: 0,
+            ),
+            // 11
+            Positioned(
+              child: Text(
+                chef,
+                style: FooderlichTheme.darkTextTheme.bodyText1,
+              ),
+              bottom: 10,
+              right: 0,
+            )
+          ],
+        ),
+        // 1
+        padding: const EdgeInsets.all(16),
+        // 2
+        constraints: const BoxConstraints.expand(
+          width: 350,
+          height: 450,
+        ),
+        // 3
+        decoration: const BoxDecoration(
+          // 4
+          image: DecorationImage(
+            // 5
+            image: AssetImage('assets/pizza.png'),
+            // 6
+            fit: BoxFit.cover,
+          ),
+          // 7
+          borderRadius: BorderRadius.all(Radius.circular(10.0)),
+        ),
+      ),
+    );
+  }
+}
